80e6df589311c4c8885909497585202218eea4b4
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-20 00:47:26 +0530

    add draw_point_history function to visualize point history on the image

--- utils/helpers.py
+++ utils/helpers.py
@@ -331,6 +331,14 @@ def draw_info_text(image, brect, handedness, hand_sign_text,
                    cv.LINE_AA)
 
     return image
+
+def draw_point_history(image, point_history):
+    for index, point in enumerate(point_history):
+        if point[0] != 0 and point[1] != 0:
+            cv.circle(image, (point[0], point[1]), 1 + int(index / 2),
+                      (152, 251, 152), 2)
+
+    return image
     
     
 
