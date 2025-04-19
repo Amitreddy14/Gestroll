43efd0c1bb5d230f51eb2a85c6a7fc5d51ac9e2e
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 23:49:22 +0530

    Enhance draw_landmarks function by adding additional circles for improved landmark visualization

--- utils/helpers.py
+++ utils/helpers.py
@@ -215,4 +215,9 @@ def draw_landmarks(image, landmark_point):
         if index == 3: 
             cv.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
                       -1)
-            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)    
+            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
+
+        if index == 4: 
+            cv.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
+                      -1)
+            cv.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
