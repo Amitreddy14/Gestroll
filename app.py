c9da978109342878692d17cb932c800925f36e48
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 11:58:03 +0530

    Reorganize import statements in app.py for improved structure and readability

--- app.py
+++ app.py
@@ -11,3 +11,12 @@ import numpy as np
 import mediapipe as mp
 
 from model import KeyPointClassifier
+
+from utils.helpers import *
+from effects.selfie_segmentation import segment_selfie
+from effects.gen_segmentation import segment_image, get_segmented_object
+from effects.point_art import *
+from effects.collect_effects import *
+
+import tensorflow as tf
+import tensorflow_hub as hub
