e905b5ebf0492566689e94e6f21e9cb6561984e0
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 14:59:08 +0530

    Implement KeyPointClassifier initialization and tensor allocation

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -9,3 +9,9 @@ class KeyPointClassifier(object):
         model_path='model/keypoint_classifier/keypoint_classifier.tflite',
         num_threads=1,
     ):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path,
+                                               num_threads=num_threads)
+
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
