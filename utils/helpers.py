c3dd75d688e8e4c1923b667dd277fa04a3032fd4
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-20 00:05:06 +0530

    improved visualization at indices 16 and 17 in draw_landmarks function

--- utils/helpers.py
+++ utils/helpers.py
@@ -280,6 +280,13 @@ def draw_landmarks(image, landmark_point):
         if index == 16:  
             cv.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255),
                       -1)
-            cv.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)  
+            cv.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
+
+        if index == 17: 
+            cv.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1) 
+
+             
 
                                       
