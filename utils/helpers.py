8d5ea76b9f22d8e98127572b1636436c4a24fa70
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 12:15:05 +0530

    Implement pre_process_landmark function to normalize landmark coordinates

--- utils/helpers.py
+++ utils/helpers.py
@@ -48,3 +48,11 @@ def calc_landmark_list(image, landmarks):
 
 def pre_process_landmark(landmark_list):
     temp_landmark_list = copy.deepcopy(landmark_list)
+
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
