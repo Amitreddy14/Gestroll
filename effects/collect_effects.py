24410a47b55235580533f1860540239fbd524091
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 15:11:44 +0530

    Fix formatting by adding newline at the end of the cartoon_effect function

--- effects/collect_effects.py
+++ effects/collect_effects.py
@@ -5,3 +5,8 @@ def cartoon_effect(frame, color_change):
     # prepare color
     if color_change:
         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    img_color = cv.pyrDown(cv.pyrDown(frame))
+    for _ in range(3):
+        img_color = cv.bilateralFilter(img_color, 9, 9, 7)
+    img_color = cv.pyrUp(cv.pyrUp(img_color))    
