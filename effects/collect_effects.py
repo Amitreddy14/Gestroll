95b7fe91dd622abfe4fceacf885b994e6db3286b
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 15:12:44 +0530

    Add edge preparation to cartoon_effect function

--- effects/collect_effects.py
+++ effects/collect_effects.py
@@ -10,3 +10,11 @@ def cartoon_effect(frame, color_change):
     for _ in range(3):
         img_color = cv.bilateralFilter(img_color, 9, 9, 7)
     img_color = cv.pyrUp(cv.pyrUp(img_color))    
+
+    # prepare edges
+    img_edges = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    img_edges = cv.adaptiveThreshold(
+        cv.medianBlur(img_edges, 7), 255,
+        cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,
+        9, 2,)
+    img_edges = cv.cvtColor(img_edges, cv.COLOR_GRAY2RGB)
