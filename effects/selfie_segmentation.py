e6fd095a9406091f31361d4ab72ec7382765b178
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 22:00:20 +0530

    Add RGB conversion and segmentation mask processing in segment_selfie function

--- effects/selfie_segmentation.py
+++ effects/selfie_segmentation.py
@@ -31,3 +31,8 @@ def segment_selfie(fg):
      # initialize mediapipe
     mp_selfie_segmentation = mp.solutions.selfie_segmentation
     selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation()
+
+    RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = selfie_segmentation.process(RGB)
+    mask = results.segmentation_mask
+    mask = cv2.GaussianBlur(mask, (33, 33), 0)
