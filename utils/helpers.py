e8ec871b44b3730524bfd38e2eeed965159d79f7
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 23:56:08 +0530

    circles for improved visualization at index 10

--- utils/helpers.py
+++ utils/helpers.py
@@ -245,4 +245,9 @@ def draw_landmarks(image, landmark_point):
         if index == 9:  
             cv.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
                       -1)
-            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)            
+            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)  
+
+        if index == 10: 
+            cv.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255),
+                      -1)
+            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)              
