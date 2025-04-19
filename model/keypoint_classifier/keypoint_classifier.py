77b2115a14dc8a8f580e3bad1ce2f09c34578875
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 14:54:02 +0530

    Add KeyPointClassifier class with initialization parameters

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import tensorflow as tf
+
+class KeyPointClassifier(object):
+    def __init__(
+        self,
+        model_path='model/keypoint_classifier/keypoint_classifier.tflite',
+        num_threads=1,
+    ):
