87918cf456a0aff2f2c69cc61dabe2d3b3828bbd
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 21:52:41 +0530

    Add selfie segmentation initialization in replace_background function

--- effects/selfie_segmentation.py
+++ effects/selfie_segmentation.py
@@ -5,3 +5,6 @@ import numpy as np
 def replace_background(fg, bg):
     bg_image = bg
     frame = fg
+
+    mp_selfie_segmentation = mp.solutions.selfie_segmentation
+    selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation()
